cb79376c36b3f23a54cdd459671cf96cfcb3ef31
GabeCr4zy <slafer_gabriel@hotmail.com>
2018-04-27 12:30:35 -0300

    Resolver a treta da api

--- src/App.js
+++ src/App.js
@@ -5,32 +5,23 @@ import './App.css';
 class App extends Component {
   componentWillMount(){
     //evita erros ao carregar a página
-    this.setState({ dados: {} });
+    this.setState({ Img: {} });
   }
-
-  aoDigitar = (event) => {
-    this.cep = event.target.value;
-  }
-
+  
+  
   aoClicar = (event) => {
-    axios.get(`https://viacep.com.br/ws/${this.cep}/json/`)
+    axios.get('https://randomfox.ca/floof/')
     .then((resposta) => {
-      this.setState({dados: resposta.data});
+      this.setState({dados: image});
     });
   }
-
+  
   render() {
     return (
       <div className="App">
         <div>
-          <input type="text" onChange={this.aoDigitar}/>
           <button onClick={this.aoClicar}>Buscar</button>
         </div>
-
-        <p>Endereço: {this.state.dados.logradouro}</p>
-        <p>Bairro: {this.state.dados.bairro}</p>
-        <p>Cidade: {this.state.dados.localidade}</p>
-        <p>Estado: {this.state.dados.uf}</p>
       </div>
     );
   }
